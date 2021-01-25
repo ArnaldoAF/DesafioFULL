2fb767268529881c44cc3cf2e41596793883047b
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2021-01-25 00:37:44 -0300

    Inserted Title ready

--- server/src/controllers/titleController.ts
+++ server/src/controllers/titleController.ts
@@ -6,7 +6,8 @@ export default class TitleController {
     async create(request: Request, response: Response) {
         console.log("---------------------------------------------------");
         console.log("TitleController - Create")
-        const { number, name, cpf, interest, penalty } = request.body;
+        const { number, name, cpf, interest, penalty, parcels } = request.body;
+        console.log(request.body);
 
         const titleObj = {
             number, 
@@ -16,25 +17,43 @@ export default class TitleController {
             penalty
         }
 
+        
+        const trx = await db.transaction();
+        
         try {
             console.log("titleObj", titleObj);
 
-            const insertedTitle = await db('titles').insert(titleObj);
+            const insertedTitle = await trx('titles').insert(titleObj);
+            const title_id = insertedTitle[0];
 
-            console.log("insertedTitle", insertedTitle);
+            console.log("title_id", title_id);
+            const parcelsList = parcels.map((x:any)=> {
+                return {
+                    title_id,
+                    ...x
+                }
+            })
+
+            console.log("parcelsList",parcelsList);
+
+
+            await trx('parcels').insert(parcelsList);
+
+            await trx.commit();
 
             return response.status(201).json({
                 "message": "´Título inserido com sucesso",
-                "body": titleObj,
-                "id": insertedTitle
+                "body": {titleObj, parcels: parcelsList},
+                "id": title_id
                 })
         } catch(err) {
             console.log(err);
+            await trx.rollback();
             return response.status(400).json({
                 "message": "Erro ao inserir Titulo",
                 "body": err
             })
-        }
+        } 
     }
     
 
